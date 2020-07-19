8e3dad749ee10cfdc3fd395f27c2dc92cf78c3ac
steven <langsford.steven@gmail.com>
2020-07-19 14:24:43 -0400

    first draw

--- public/js/exp.js
+++ public/js/exp.js
@@ -2,6 +2,17 @@
 //If that's all you want, all you need to edit is the makeTrial object and the responseListener. Give maketrial an appropriate constructor that accept the key trial properties, a drawMe function, and something that will hit responseListener.
 //then put a list of trial-property-setter entries in 'stim' and you're golden.
 
+function shuffle(a) { //via https://stackoverflow.com/questions/6274339/how-can-i-shuffle-an-array
+    var j, x, i;
+    for (i = a.length - 1; i > 0; i--) {
+        j = Math.floor(Math.random() * (i + 1));
+        x = a[i];
+        a[i] = a[j];
+        a[j] = x;
+    }
+    return a;
+}
+
 var trials = [];
 var trialindex = 0;
 
@@ -31,32 +42,108 @@ function nextTrial(){
 //the data-getting process in 'dashboard.ejs' & getData routes creates a csv with a col for every attribute, using 'Object.keys' to list all the properties of the object. Assumes a pattern where everything interesting is saved to the trial object, then that is JSONified and saved as a response.
 //Note functions are dropped by JSON.
 //Also note this means you have to be consistent with the things that are added to each trial before they are saved, maybe init with NA values in the constructor.
-function makeTrial(questiontext){
+
+function makeOption(prob, pay, role, itemtype){
+    this.prob = prob;
+    this.pay = pay;
+    this.itemtype = itemtype;
+    this.role = role;
+
+    this.drawMe = function(x, y){
+	console.log("Drew: "+prob+":"+pay+":"+itemtype)
+    }
+}
+
+function makeTrial(targ, comp, decoy, bg){
+    this.targ = targ;
+    this.comp = comp;
+    this.decoy = decoy;
+    this.bg = bg;
+
+    this.pres_order = shuffle([1,2,3]);
+    
     this.ppntID = localStorage.getItem("ppntID");
-    this.questiontext = questiontext;
     this.drawMe = function(targdiv){
 	this.drawTime = Date.now();
-	var responses = "<button onclick='responseListener(\"yes\")'>Yes</button><button onclick='responseListener(\"no\")'>No</button>";
-	document.getElementById(targdiv).innerHTML=
-	    "<div class='trialdiv'><p>"+this.questiontext+"</br>"+responses+"</p></div>";
+
+	targ.drawMe(0,0);
+	comp.drawMe(10,10);
+	decoy.drawMe(20,20);
+	// var responses = "<button onclick='responseListener(\"yes\")'>Yes</button><button onclick='responseListener(\"no\")'>No</button>";
+	// document.getElementById(targdiv).innerHTML=
+	//     "<div class='trialdiv'><p>"+this.questiontext+"</br>"+responses+"</p></div>";
     }
 }
 
 
-
-function shuffle(a) { //via https://stackoverflow.com/questions/6274339/how-can-i-shuffle-an-array
-    var j, x, i;
-    for (i = a.length - 1; i > 0; i--) {
-        j = Math.floor(Math.random() * (i + 1));
-        x = a[i];
-        a[i] = a[j];
-        a[j] = x;
-    }
-    return a;
-}
 //****************************************************************************************************
 //Stimuli
-var stim = shuffle(["Does this question have a correct answer?","What is the correct answer to this question?"]);
-trials = stim.map(function(x){return new makeTrial(x)});
+//var stim = shuffle(["Does this question have a correct answer?","What is the correct answer to this question?"]);
+//trials = stim.map(function(x){return new makeTrial(x)});
 
-nextTrial();
+trials = [
+    new makeTrial(
+	targ = new makeOption(prob=.1, pay=10, role="targ", itemtype="urn"),
+	comp = new makeOption(prob=.2, pay=20, role="comp", itemtype="ticket"),
+	decoy = new makeOption(prob=.3, pay=30, role="decoy", itemtype="urn"),
+	bg = "urn"),
+    new makeTrial(
+	targ = new makeOption(prob=.4, pay=40, role="targ", itemtype="urn"),
+	comp = new makeOption(prob=.5, pay=50, role="comp", itemtype="ticket"),
+	decoy = new makeOption(prob=.6, pay=100, role="decoy", itemtype="urn"),
+	bg = "urn")
+]
+
+//nextTrial();
+
+function trial_drawstring(targ, comp, decoy, pres_order){
+
+function stim_drawstring(left, top, img, prob, pay){
+    return(
+	"<span class='stimplacer' style='left:"+left+"px; top:"+top+"px'>"+
+"<figure class='img-set'>"+
+  "<img src='img/"+img+".png'"+
+    "width='100' height='100'"+
+    ">"+
+  "<figcaption>"+prob+" chance<br>"+
+    "of winning $"+pay+""+
+    "</figcaption>"+
+    "</figure>"+
+	    "</span>"
+    )
+}
+    
+    //cross-browser thingamibob via https://stackoverflow.com/questions/1248081/how-to-get-the-browser-viewport-dimensions
+    const vw = Math.max(document.documentElement.clientWidth || 0, window.innerWidth || 0)
+    const vh = Math.max(document.documentElement.clientHeight || 0, window.innerHeight || 0)
+    const imgsize = 100; //magic number :-( re-center imgs, [top / left] refs upper left corner
+
+    draw_cords = [
+	[
+	    vh / 3 - imgsize,//const position1_top 
+	    vw / 3 - imgsize,//const position1_left 
+	],
+	[
+	    vh / 3 -imgsize,//const position2_top 
+	    vw * 2 / 3 -imgsize,//const position2_left 
+	],
+	[
+	    vh *2 / 3 -imgsize/2,//const position3_top 
+	    vw / 2 -imgsize,//const position3_left 
+	]
+    ]
+
+    console.log(draw_cords[pres_order[0]][0])
+    return(
+	stim_drawstring(draw_cords[pres_order[0]][1], draw_cords[pres_order[0]][0], targ.itemtype, targ.prob, targ.pay)+
+	    stim_drawstring(draw_cords[pres_order[1]][1], draw_cords[pres_order[1]][0], comp.itemtype, comp.prob, comp.pay)+
+	    stim_drawstring(draw_cords[pres_order[2]][1], draw_cords[pres_order[2]][0], decoy.itemtype, decoy.prob, decoy.pay)
+    )
+    
+}
+document.getElementById("uberdiv").innerHTML = trial_drawstring(
+    new makeOption(prob=.1, pay=10, role="targ", itemtype="urn"),
+    new makeOption(prob=.2, pay=20, role="comp", itemtype="ticket"),
+    new makeOption(prob=.3, pay=30, role="decoy", itemtype="urn"),
+    [2,1,0]
+)
